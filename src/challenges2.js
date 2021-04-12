0a3ccaec83c5d1477b403bf572d64eaae2f6402f
Júlio Mota <juliomotacampos@gmail.com>
2021-04-12 13:50:29 -0300

    Adicionado requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -28,93 +28,93 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-// function checkPhoneNumberLenght(phoneNumber) {
-//   let isValid = false;
+function checkPhoneNumberLenght(phoneNumber) {
+  let isValid = false;
 
-//   if (phoneNumber.length !== 11) {
-//     isValid = false;
-//   } else {
-//     isValid = true;
-//   }
+  if (phoneNumber.length !== 11) {
+    isValid = false;
+  } else {
+    isValid = true;
+  }
 
-//   return isValid;
-// }
+  return isValid;
+}
 
-// function checkPhoneNumberDigits(phoneNumber) {
-//   let isValid = false;
-//   let count = 0;
+function checkPhoneNumberDigits(phoneNumber) {
+  let isValid = false;
+  let count = 0;
 
-//   for (let number of phoneNumber) {
-//     if (number < 0 || number > 9) {
-//       count += 1;
-//     }
-//   }
+  for (let number of phoneNumber) {
+    if (number < 0 || number > 9) {
+      count += 1;
+    }
+  }
 
-//   if (count === 0) {
-//     isValid = true;
-//   } else {
-//     isValid = false;
-//   }
+  if (count === 0) {
+    isValid = true;
+  } else {
+    isValid = false;
+  }
 
-//   return isValid;
-// }
+  return isValid;
+}
 
-// function checkPhoneNumberRepetitions(phoneNumber) {
-//   let isValid = false;
-//   let mostRepetitions = 0;
+function checkPhoneNumberRepetitions(phoneNumber) {
+  let isValid = false;
+  let mostRepetitions = 0;
 
-//   for (let number of phoneNumber) {
-//     let repetitions = 0;
+  for (let number of phoneNumber) {
+    let repetitions = 0;
 
-//     for (let index of phoneNumber) {
-//       if (index === number) {
-//         repetitions += 1;
-//       }
-//     }
+    for (let index of phoneNumber) {
+      if (index === number) {
+        repetitions += 1;
+      }
+    }
 
-//     if (repetitions > mostRepetitions) mostRepetitions = repetitions;
-//   }
+    if (repetitions > mostRepetitions) mostRepetitions = repetitions;
+  }
 
-//   if (mostRepetitions < 3) {
-//     isValid = true;
-//   } else {
-//     isValid = false;
-//   }
+  if (mostRepetitions < 3) {
+    isValid = true;
+  } else {
+    isValid = false;
+  }
 
-//   return isValid;
-// }
+  return isValid;
+}
 
-// function numberCheck(phoneNumber) {
-//   let answer = '';
+function numberCheck(phoneNumber) {
+  let answer = '';
 
-//   if (checkPhoneNumberLenght(phoneNumber) === false) {
-//     answer = 'Array com tamanho incorreto';
-//   } else if (checkPhoneNumberDigits(phoneNumber) === false || checkPhoneNumberRepetitions(phoneNumber) === false) {
-//     answer = 'Não é possível gerar um número de telefone com esses valores.'
-//   } else {
-//     answer = 'ok';
-//   }
+  if (checkPhoneNumberLenght(phoneNumber) === false) {
+    answer = 'Array com tamanho incorreto';
+  } else if (checkPhoneNumberDigits(phoneNumber) === false || checkPhoneNumberRepetitions(phoneNumber) === false) {
+    answer = 'Não é possível gerar um número de telefone com esses valores.'
+  } else {
+    answer = 'ok';
+  }
 
-//   return answer;
-// }
+  return answer;
+}
 
-function generatePhoneNumber() {
-  // let answer = '';
+function generatePhoneNumber(phoneNumber) {
+  let answer = '';
 
-  // if (numberCheck(phoneNumber) !== 'ok') {
-  //   answer = numberCheck(phoneNumber);
-  // } else {
-  //   let stringNumber = '';
+  if (numberCheck(phoneNumber) !== 'ok') {
+    answer = numberCheck(phoneNumber);
+  } else {
+    let stringNumber = '';
 
-  //   for (let number of phoneNumber) {
-  //     stringNumber += number;
-  //   }
+    for (let number of phoneNumber) {
+      stringNumber += number;
+    }
 
-  //   stringNumber = stringNumber.replace(/(\d{2})?(\d{5})?(\d{4})/, '($1) $2-$3');
-  //   answer = stringNumber;
-  // }
+    stringNumber = stringNumber.replace(/(\d{2})?(\d{5})?(\d{4})/, '($1) $2-$3');
+    answer = stringNumber;
+  }
 
-  // return answer;
+  return answer;
 }
 
 // Desafio 12
