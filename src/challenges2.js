c7bb2404f64b16838aadbbd88e2e2aecf16e3232
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-04-20 13:02:30 -0300

    verificaçao de 11 requisitos

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,8 +21,27 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
+function testSmaller(lineA, lineB, lineC) {
   // seu código aqui
+  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB) {
+    return true;
+  }
+  return false;
+}
+function testBigger(lineA, lineB, lineC) {
+  let testLineA = Math.abs(lineB - lineC);
+  let testLineB = Math.abs(lineA - lineC);
+  let testlineC = Math.abs(lineA - lineB);
+  if (lineA > testLineA && lineB > testLineB && lineC > testlineC) {
+    return true;
+  }
+  return false;
+}
+function triangleCheck(lineA, lineB, lineC) {
+  if (testSmaller(lineA, lineB, lineC) && testBigger(lineA, lineB, lineC)){
+    return true;
+  }
+  return false;
 }
 
 // Desafio 13
