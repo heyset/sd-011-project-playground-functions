9744b23776acabe5ed7d4b3f408c7baf30daf1dc
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-04-20 13:35:42 -0300

    verificaçao de 12 requisitos prontos

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,8 +16,45 @@ function techList(selectedTech, nameOfUser) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function checkRepeat(value, phone) {
+  let repetiu = 0;
+  for (let indexNumber = 0; indexNumber < phone.length; indexNumber += 1) {
+    if (value === phone[indexNumber]) {
+      repetiu += 1;
+    }
+    if (repetiu >= 3) {
+      return true;
+    }
+  }
+  return false;
+}
+function checkOtherThanEleven(value) {
+  return value < 0 || value > 9;
+}
+function testPhoneNumber(phone) {
+  if (phone.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let testIndex = 0; testIndex < phone.length; testIndex += 1) {
+    if (checkOtherThanEleven(phone[testIndex]) || checkRepeat(phone[testIndex], phone)) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+}
+function generatePhoneNumber(phone) {
+  if (testPhoneNumber(phone)) {
+    return testPhoneNumber(phone);
+  }
+  let generateNumber = '(';
+  for (let number = 0; number < phone.length; number += 1) {
+    if (generateNumber.length === 3) {
+      generateNumber += ') ';
+    } else if (generateNumber.length === 10) {
+      generateNumber += '-';
+    }
+    generateNumber += phone[number];
+  }
+  return generateNumber;
 }
 
 // Desafio 12
