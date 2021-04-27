695208fded5df71ee0f8f9b884341941d9a83a23
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-27 01:11:03 -0300

    11 continua

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,36 +19,38 @@ function techList(tecnologias, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber(numero) {
-  // seu código aqui
-  let check = true; let retornar = '';
+let retornar = '';
+let check = true;
+let contador = 0;
+let numero = [];
+function avaliaNumero(tamanho) {
+  for (let i = 0; i < tamanho; i += 1) {
+    for (let j = 0; j < tamanho; j += 1) {
+      if (numero[i] === numero[j]) {
+        contador += 1;
+      }
+    }
+    if (numero[i] < 0 || numero[i] > 9 || contador >= 3) {
+      check = false;
+      retornar = 'não é possível gerar um número de telefone com esses valores';
+    }
+    contador = 0;
+  }
+}
+function generatePhoneNumber(array) {
+  numero = array;
   if (numero.length !== 11) {
     check = false;
     retornar = 'Array com tamanho incorreto.';
-  } else {
-    for (let i = 0; i < numero.length; i += 1) {
-      let contador = 0;
-      for (let j = 0; j < numero.length; j += 1) {
-        if (numero[i] === numero[j]) {
-          contador += 1;
-        }
-      }
-      if (numero[i] < 0 || numero[i] > 9 || contador >= 3) {
-        check = false;
-        retornar = 'não é possível gerar um número de telefone com esses valores';
-      }
-    }
-    if (check) {
-      let telefone = []; telefone[0] = '('; telefone[1] = numero[0];
-      telefone[2] = numero[1]; telefone[3] = ') ';
-      telefone[4] = numero[2]; telefone[5] = numero[3];
-      telefone[6] = numero[4]; telefone[7] = numero[5];
-      telefone[8] = numero[6]; telefone[9] = '-';
-      telefone[10] = numero[7]; telefone[11] = numero[8];
-      telefone[12] = numero[9]; telefone[13] = numero[10];
-      for (let i = 0; i < telefone.length; i += 1) {
-        retornar += telefone[i];
-      }
+  }
+  if (check) {
+    avaliaNumero(numero.length);
+  }
+  if (check) {
+    let telefone = ['(', numero[0], numero[1], ')', numero[2], numero[3], numero[4], numero[5],
+      numero[6], '-', numero[7], numero[8], numero[9], numero[10]];
+    for (let i = 0; i < telefone.length; i += 1) {
+      retornar += telefone[i];
     }
   }
   return retornar;
