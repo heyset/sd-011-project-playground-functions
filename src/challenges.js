57f869b5d93bd37062632142b640158817327889
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-04-29 11:08:07 -0300

    refatorando functions

--- src/challenges.js
+++ src/challenges.js
@@ -1,39 +1,17 @@
-// Desafio 1
-// Para este desafio utilizei o que aprendi em aula.
-function compareTrue(valorOne, ValorTwo) {
-  return valorOne && ValorTwo;
-}
+const compareTrue = (valorOne, ValorTwo) => valorOne && ValorTwo;
 
-// Desafio 2
-// Para este desafio utilizei o que aprendi em aula.
-function calcArea(base, height) {
-  return (base * height) / 2;
-}
+const calcArea = (base, height) => (base * height) / 2;
 
-// Desafio 3
-// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/split
-function splitSentence(SeparateWords) {
-  return (SeparateWords.split(' '));
-}
+const splitSentence = (SeparateWords) => (SeparateWords.split(' '));
 
-// Desafio 4
-// Para este desafio utilizei o que aprendi em aula.
-// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Template_literals
-function concatName(myArray) {
-  return `${myArray[myArray.length - 1]}, ${myArray[0]}`;
-}
+const concatName = (myArray) => `${myArray[myArray.length - 1]}, ${myArray[0]}`;
 
-// Desafio 5
-// Para este desafio utilizei o que aprendi em aula.
-function footballPoints(wins, ties) {
+const footballPoints = (wins, ties) => {
   let points = wins * 3 + ties * 1;
   return points;
-}
+};
 
-// Desafio 6
-// Para este desafio utilizei o que aprendi em aula.
-// https://pt.stackoverflow.com/questions/35202/como-saber-o-maior-valor-de-um-array
-function highestCount(listOfNumbers) {
+const highestCount = (listOfNumbers) => {
   let count = 0;
   let highestNumber = Math.max.apply(null, listOfNumbers);
   for (let index = 0; index < listOfNumbers.length; index += 1) {
@@ -42,11 +20,9 @@ function highestCount(listOfNumbers) {
     }
   }
   return count;
-}
+};
 
-// Desafio 7
-// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
-function catAndMouse(mouse, cat1, cat2) {
+const catAndMouse = (mouse, cat1, cat2) => {
   if (Math.abs(cat1 - mouse) > (cat2 - mouse)) {
     return 'cat2';
   }
@@ -54,11 +30,9 @@ function catAndMouse(mouse, cat1, cat2) {
     return 'cat1';
   }
   return 'os gatos trombam e o rato foge';
-}
+};
 
-// Desafio 8
-// Para este desafio utilizei o que aprendi em aula.
-function fizzBug(divArray) {
+const fizzBug = (divArray) => {
   for (let index = 0; index < divArray.length; index += 1) {
     if (divArray[index] % 3 === 0) {
       divArray[index] = 'fizz';
@@ -67,9 +41,9 @@ function fizzBug(divArray) {
     }
   }
   return divArray;
-}
+};
 
-function fizzBuzz(divArray) {
+const fizzBuzz = (divArray) => {
   for (let index = 0; index < divArray.length; index += 1) {
     if (divArray[index] % 3 === 0 && divArray[index] % 5 === 0) {
       divArray[index] = 'fizzBuzz';
@@ -79,27 +53,25 @@ function fizzBuzz(divArray) {
   }
   divArray = fizzBug(divArray);
   return divArray;
-}
+};
 
-// Desafio 9
-// https://www.w3schools.com/jsref/jsref_replace.asp
-function encode(encondeParam) {
+const encode = (encondeParam) => {
   encondeParam = encondeParam.replace(/a/g, '1');
   encondeParam = encondeParam.replace(/e/g, '2');
   encondeParam = encondeParam.replace(/i/g, '3');
   encondeParam = encondeParam.replace(/o/g, '4');
   encondeParam = encondeParam.replace(/u/g, '5');
   return encondeParam;
-}
+};
 
-function decode(decodeParam) {
+const decode = (decodeParam) => {
   decodeParam = decodeParam.replace(/1/g, 'a');
   decodeParam = decodeParam.replace(/2/g, 'e');
   decodeParam = decodeParam.replace(/3/g, 'i');
   decodeParam = decodeParam.replace(/4/g, 'o');
   decodeParam = decodeParam.replace(/5/g, 'u');
   return decodeParam;
-}
+};
 
 module.exports = {
   calcArea,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,4 @@
-// Desafio 10
-// para o entendimento desse desafio precisei comparar o código de colegas no slack
-// https://trybecourse.slack.com/archives/C01PLFW7347/p1617330837144600
-// https://github.com/tryber/sd-011-project-playground-functions/pull/168/files
-// vendo o codigo do guilheme pereira no github, ficou muito claro o entendimento desse desafio
-function techList(tech, name) {
+const techList = (tech, name) => {
   tech = tech.sort();
   for (let index = 0; index < tech.length; index += 1) {
     tech[index] = {
@@ -15,13 +10,9 @@ function techList(tech, name) {
     tech = 'Vazio!';
   }
   return tech;
-}
+};
 
-// Desafio 11
-// https://learnersbucket.com/examples/javascript/how-to-format-phone-number-in-javascript/
-// para divinuir a complexidade do meu código, comparei com de colegas no github
-// https://github.com/tryber/sd-011-project-playground-functions/pull/160/files
-function more3(numberArray, x) {
+const more3 = (numberArray, x) => {
   let counts = 0;
   for (let i = 0; i <= numberArray.length; i += 1) {
     if (numberArray[i] === x) {
@@ -29,35 +20,31 @@ function more3(numberArray, x) {
     }
   }
   return counts;
-}
+};
 
-function byNineNumber(numberArray) {
+const byNineNumber = (numberArray) => {
   for (let i of numberArray) {
     if (i < 0 || i > 9 || more3(numberArray, i) >= 3) {
       return false;
     }
   }
-}
+};
 
-function generatePhoneNumber(numberArray) {
+const generatePhoneNumber = (numberArray) => {
   if (numberArray.length !== 11) {
     return `${'Array com tamanho incorreto.'}`;
   }
   if (byNineNumber(numberArray) === false) {
     return `${'não é possível gerar um número de telefone com esses valores'}`;
   }
-  let cleaned = `${(numberArray)}`.replace(/\D/g, ''); // Filter only numbers from the input
-  let match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/); // Check if the input is of correct length
+  let cleaned = `${(numberArray)}`.replace(/\D/g, '');
+  let match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
   if (match) {
     return `(${match[1]}) ${match[2]}-${match[3]}`;
   }
-}
+};
 
-// Desafio 12
-// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
-// para diminuir a complexidade do meu código,comparei com o codigo do colega vitor cardoso
-// https://github.com/tryber/sd-011-project-playground-functions/pull/77/files
-function triangleCheck(lineA, lineB, lineC) {
+const triangleCheck = (lineA, lineB, lineC) => {
   let lines1 = Math.abs(lineA + lineB);
   let lines2 = Math.abs(lineA - lineB);
   let value = false;
@@ -65,11 +52,9 @@ function triangleCheck(lineA, lineB, lineC) {
     value = true;
   }
   return value;
-}
+};
 
-// Desafio 13
-// https://www.youtube.com/watch?v=pfkkdzeyx6U&ab_channel=AllThingsJavaScript%2CLLC
-function hydrate(drinks) {
+const hydrate = (drinks) => {
   let drinksReturn = /\d+/g;
   let cupsOfWater = 0;
   drinks = drinks.match(drinksReturn);
@@ -80,7 +65,7 @@ function hydrate(drinks) {
     return `${cupsOfWater} copo de água`;
   }
   return `${cupsOfWater} copos de água`;
-}
+};
 
 module.exports = {
   generatePhoneNumber,
