367c7be66a36fca825c1606e45174be19abc9512
Kevin Willian <kevinwillian084@gmail.com>
2021-05-28 17:39:49 -0300

    Refatora exercicios do 1 ao 6 com ES6

--- src/challenges.js
+++ src/challenges.js
@@ -1,80 +1,65 @@
 // Desafio 1
-function compareTrue(bool1, bool2) {
-  return bool1 && bool2
-}
+const compareTrue = (bool1, bool2) => bool1 && bool2;
 console.log(compareTrue(true, false));
 
 // Desafio 2
-function calcArea(base, height) {
-  return base * height/2
-}
+const calcArea = (base, height) => base * (height / 2);
 console.log(calcArea(51, 1));
 
 // Desafio 3
-function splitSentence(originalString) { 
-  return originalString.split(' ');
-}
+const splitSentence = (originalString) => originalString.split(' ');
 console.log(splitSentence('vamo que vamo'));
 
 // Desafio 4
-function concatName(stringExterna) {
+const concatName = (stringExterna) => {
   let arrai = stringExterna;
-  return arrai[arrai.length-1] + ', ' + arrai[0]
-}
+  return `${arrai[arrai.length - 1]}, ${arrai[0]}`;
+};
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
 
 // Desafio 5
-function footballPoints(wins, ties) {
-  return wins * 3 + ties;
-}
+const footballPoints = (wins, ties) => wins * 3 + ties;
 console.log(footballPoints(0, 0));
 
 // Desafio 6
 function highestCount(arrayDeNumeros) {
-  let sortedArray = (arrayDeNumeros.sort()); 
-  let highestNumber = sortedArray[sortedArray.length-1]; 
-  let frequencyHighest = 0;
-  for(let index = 0; index < arrayDeNumeros.length; index += 1){
-    let verifiedNumber = arrayDeNumeros[index];
-    if (highestNumber === verifiedNumber){
-      frequencyHighest += 1;
-    }
-  }
-  return frequencyHighest
-  }
-  console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
-  
+  return arrayDeNumeros.sort((a, b) => a < b).reduce((acc, number) => {
+    acc = arrayDeNumeros[0] === number ? acc += 1 : acc;
+    return acc;
+  }, 0);
+}
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let catWinner ='';  
   let cat1Distance = Math.abs(mouse - cat1);
   let cat2Distance = Math.abs(mouse - cat2);
 
-  if(cat1Distance < cat2Distance){
+  if (cat1Distance < cat2Distance) {
     catWinner = 'cat1';
-  }else if(cat2Distance < cat1Distance){
+  } else if (cat2Distance < cat1Distance) {
     catWinner = 'cat2';
-  }else if(cat1Distance === cat2Distance){
+  } else if (cat1Distance === cat2Distance) {
     catWinner = 'os gatos trombam e o rato foge';
   }
-  return catWinner
+  return catWinner;
 }
 console.log(catAndMouse(10, 22, 22));
 
 // Desafio 8
 function fizzBuzz(arrai) {
   let hello = [];
-  for (let index = 0; index < arrai.length; index += 1){
-    if(arrai[index] % 15 === 0){
+  for (let index = 0; index < arrai.length; index += 1) {
+    if (arrai[index] % 15 === 0) {
       hello.push('fizzBuzz');
-    }else if (arrai[index] % 3 === 0){
+    } else if (arrai[index] % 3 === 0) {
       hello.push('fizz');
-    }else if (arrai[index] % 5 === 0){
+    } else if (arrai[index] % 5 === 0) {
       hello.push('buzz');
-    }else {
+    } else {
       hello.push('bug!');
     }
-   
   }
   return hello;
 }
@@ -83,7 +68,7 @@ console.log(fizzBuzz([9, 25]));
 // Desafio 9
 function encode(string) {
   let newString = string; 
-  for (index of string) {
+  for (let index of string) {
     newString = newString.replace('a', '1');
     newString = newString.replace('e', '2');
     newString = newString.replace('i', '3');
